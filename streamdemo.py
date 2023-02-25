e36a8bd68863fffdac15f048db6c03922001c21e
Altaf Rehmani <altafr@Altafs-MacBook-Air.local>
2023-02-25 22:14:10 +0800

    remove files

--- streamdemo.py
+++
@@ -1,7 +0,0 @@
-from langchain.llms import OpenAI
-from langchain.callbacks.base import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
-
-llm = OpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature=0)
-resp = llm("Write me a song about friends on a trip to hawaii.")
